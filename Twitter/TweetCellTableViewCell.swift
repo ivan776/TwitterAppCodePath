2da3a6ff610098019abe79bb58ccac3fd044f526
ivan776 <mendozaivan1224@gmail.com>
2022-03-01 15:44:10 -0800

    Twitter App
    App using Twitter API to load tweets

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Ivan Mendoza on 2/22/22.
+//  Copyright © 2022 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        // Configure the view for the selected state
+    }
+
+}
